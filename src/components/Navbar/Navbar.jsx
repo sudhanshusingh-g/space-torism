848030deadee5f0b61922e195a4e711a03e0cd51
sudhanshusingh-g <sudsingh32@gmail.com>
2024-09-27 12:50:49 +0530

    Home page done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,49 +1,53 @@
-import { Menu } from "lucide-react"
-import Logo from "../Logo/Logo.jsx"
-import { Link } from "react-router-dom"
+import { Menu } from "lucide-react";
+import Logo from "../Logo/Logo.jsx";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <header className="absolute w-full py-8 flex items-center justify-between">
       <Logo />
-      <div className="w-[40%] border-t-[1px] border-white opacity-95  hidden lg:block"></div>
+      <div className="w-[40%] border-t-[1px] border-white opacity-95 hidden lg:block"></div>
 
       <nav
-        className="hidden md:block bg-white/10 backdrop-blur-lg border border-white/20 w-[50%]
+        className="hidden md:block bg-white/10 backdrop-blur-lg border border-white/20 lg:w-[50%]
       rounded-l-md py-4 px-4"
       >
         <ul className="flex items-center justify-center space-x-8 text-white">
-          <li>
+          <li className="relative group">
             <Link to="/" className="font-barlow uppercase">
               <span className="mr-1 font-bold">00</span>
               <span className="font-[300]">Home</span>
             </Link>
+            <span className="absolute left-0 -bottom-4 h-[2px] w-0 bg-white transition-all duration-300 group-hover:w-full"></span>
           </li>
-          <li>
+          <li className="relative group">
             <Link to="/destination" className="font-barlow uppercase">
               <span className="mr-1 font-bold">01</span>
               <span className="font-[300]">Destination</span>
             </Link>
+            <span className="absolute left-0 -bottom-4 h-[2px] w-0 bg-white transition-all duration-300 group-hover:w-full"></span>
           </li>
-          <li>
+          <li className="relative group">
             <Link to="/crew" className="font-barlow uppercase">
               <span className="mr-1 font-bold">02</span>
               <span className="font-[300]">Crew</span>
             </Link>
+            <span className="absolute left-0 -bottom-4 h-[2px] w-0 bg-white transition-all duration-300 group-hover:w-full"></span>
           </li>
-          <li>
+          <li className="relative group">
             <Link to="/technology" className="font-barlow uppercase">
               <span className="mr-1 font-bold">03</span>
               <span className="font-[300]">Technology</span>
             </Link>
+            <span className="absolute left-0 -bottom-4 h-[2px] w-0 bg-white transition-all duration-300 group-hover:w-full"></span>
           </li>
         </ul>
       </nav>
-      <button className="md:hidden">
-        <Menu className="text-white w-7 h-7" />
+      <button className="md:hidden mr-4">
+        <Menu className="text-white w-7 h-7"/>
       </button>
     </header>
   );
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
-    <div className="h-screen bg-home-mobile md:bg-home-tablet lg:bg-home-desktop bg-cover bg-center">
+    <div className="h-screen flex flex-col items-center justify-end bg-home-mobile md:bg-home-tablet lg:bg-home-desktop bg-cover bg-center">
+      <div className="h-fit lg:h-4/5 flex items-center flex-col justify-center p-4 lg:justify-end lg:p-8">
+        <div className="flex items-center flex-col lg:flex-row lg:space-x-16">
+          <div className="lg:w-[48%] leading-normal text-center lg:text-left space-y-0 lg:space-y-0">
+            <p className="text-blue-800 uppercase text-[1rem] lg:text-[1.25rem] font-barlow font-light tracking-widest">
+              So you want to travel to
+            </p>
+            <h1 className="text-white text-[5rem] md:text-[7rem] lg:text-[9rem] uppercase font-bellefair">
+              Space
+            </h1>
+            <p className="text-blue-800 text-[0.9rem] md:text-[1rem] lg:text-[1.125rem] font-barlow font-[300] max-w-[28rem] lg:max-w-none">
+              Let's face it; if you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we'll give you a truly out of
+              this world experience!
+            </p>
+          </div>
+          <div className="flex-1 flex items-center justify-center lg:justify-end mt-8 lg:mt-0">
+            <div className="h-60 w-60 md:h-72 md:w-72 rounded-full hover:bg-white/20 flex items-center justify-center transition-bg duration-300 hover:animate-pulse">
+              <button className="h-40 w-40 md:h-48 md:w-48 bg-white rounded-full font-bellefair text-3xl md:text-4xl transition-transform duration-300 transform hover:scale-105 focus:scale-105">
+                Explore
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default Home
+export default Home;
